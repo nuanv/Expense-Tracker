b5c347417a35720d31402f5dc35c440e3bc74523
NiharPatel <73577147+niharPat@users.noreply.github.com>
2024-05-13 19:18:20 +0530

    Delete backend directory

--- backend/routes/repRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const repRouter = express.Router();
-const { getReport } = require("../controllers/repController");
-
-repRouter.get("/get/report", getReport);
-
-module.exports = repRouter;
