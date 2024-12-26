cedfad6238ed26323d84cdbd487c904e77b1ebcd
Nihar <niharp362004.ni@gmail.com>
2024-12-26 16:03:33 +0530

    Change default PORT number

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,12 +8,16 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
- 
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
   },
 })
